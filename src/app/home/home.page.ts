b5d0183062b5e76e48a861d4fd2d47a8ace60dee
Bruno de Oliveira Nunes <141058085+BrunoNunes100@users.noreply.github.com>
2025-03-12 19:23:50 +0000

    aula 12-03

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,45 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-    pokemon: any = {
-      nome: null,
-      poder: null
+  perfil: any = {
+    foto: null,
+    nome: null,
+    profissao: null,
+    nome_usurario: null,
+    idioma: null,
+    localidade: null,
+    data_inicio: null,
+    biografia: null,
+    estatisticas: {
+      curtidas: 0,
+      seguindo: 0,
+      amigos: 0,
+    },
+    postagens: [
+      {
+        foto: 'https://i.ytimg.com/vi/t6PE6jfpBZ4/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGDogSihyMA8=&rs=AOn4CLCIGe7OKpFxVO3WOI6mjpp6jdt-OQ',
+        nome: 'Sonic The Hedghog',
+        nome_usuario: '@sonicthehedghog',
+        texto: 'Sonic Impactado',
+        data: '12/03/2025 14:00',
+      },
+      {
+        foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS--lF6akd9IHXYkFpYn7UgPOulKNMf8AFgDw&s',
+        nome: 'Sonic The Hedghog 2',
+        nome_usuario: '@sonicthehedghog2',
+        texto: 'Sonic Gordo',
+        data: '12/03/2025 14:10',
+      },
+      {
+        foto: 'https://www.adrenaline.com.br/wp-content/uploads/2022/05/sonic-feio-filme-tico-e-teco_2.jpg',
+        nome: 'Sonic The Hedghog 3',
+        nome_usuario: '@@sonicthehedghog3',
+        texto: 'Sonic Descolado',
+        data: '12/03/2025 14:20',
+      }
+    ]
+  }
+
+  constructor(){}
+  
 };
-
-  constructor(public crudService: CrudService 
-
-   ){ }
-
-
-    enviar(){
-        this.crudService.insert(this.pokemon, 'pokemons')
-    }
-}
